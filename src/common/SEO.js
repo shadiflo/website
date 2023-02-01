db15357257d706e95aa95f11770b38ec53e9151c
shadiflo <92731974+shadiflo@users.noreply.github.com>
2023-02-01 19:52:38 +0100

    update

--- src/common/SEO.js
+++ src/common/SEO.js
@@ -6,7 +6,7 @@ const SEO = ( {title} ) => {
     return (
         <>
             <meta charSet="utf-8" />
-            <title>ClanBase | {title}</title>
+            <title>ClanBase| {title}</title>
             <meta name="robots" content="noindex, follow" />
             <meta name="description" content="Creative Agency, Corporate and Portfolio React JS Template" />
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
