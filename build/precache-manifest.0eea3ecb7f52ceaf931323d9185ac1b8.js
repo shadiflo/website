ebeda675694a462f7d5b9ea77b01f453acacc67e
shadiflo <92731974+shadiflo@users.noreply.github.com>
2023-02-01 17:07:04 +0100

    update

---
+++ build/precache-manifest.0eea3ecb7f52ceaf931323d9185ac1b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c93cddc3500fdb0888a",
+    "url": "/static/css/main.eb1648d7.chunk.css"
+  },
+  {
+    "revision": "3c93cddc3500fdb0888a",
+    "url": "/static/js/main.e13daab1.chunk.js"
+  },
+  {
+    "revision": "85dbeb6abf83115a29b6",
+    "url": "/static/js/runtime~main.70091e6f.js"
+  },
+  {
+    "revision": "14d1456b3ca1288df5aa",
+    "url": "/static/css/2.7f84b9a6.chunk.css"
+  },
+  {
+    "revision": "14d1456b3ca1288df5aa",
+    "url": "/static/js/2.3735fec3.chunk.js"
+  },
+  {
+    "revision": "d03d05430b9a42cc73e3",
+    "url": "/static/js/3.06a4cbf0.chunk.js"
+  },
+  {
+    "revision": "b7100ea3e457a54afbd66866021b3bea",
+    "url": "/index.html"
+  }
+];
