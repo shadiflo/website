33fa2a12fcfd05d5cb54d11332bebdaaf851936e
shadiflo <92731974+shadiflo@users.noreply.github.com>
2023-02-08 15:30:57 +0100

    updates

--- src/common/header/Nav.js
+++ src/common/header/Nav.js
@@ -12,23 +12,20 @@ const Nav = () => {
                     <ul className="axil-submenu">
                         <li><Link to={process.env.PUBLIC_URL + "/faceithubs"}>Faceit Hubs</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/faceitvisuals"}>Faceit Extension</Link></li>
-                        <li><Link to={process.env.PUBLIC_URL + "/home-startup"}>Home Startup</Link></li>
-                        <li><Link to={process.env.PUBLIC_URL + "/corporate-agency"}>Corporate Agency</Link></li>
-                        <li><a href="https://new.axilthemes.com/demo/react/abstrak-rtl/">RTL Demo</a></li>
+                      
                     </ul>
                 </li>
                 <li className="menu-item-has-children">
                     <Link to="#">Services <FaAngleDown /></Link>
                     <ul className="axil-submenu">
                         <li><Link to={process.env.PUBLIC_URL + "/service-one"}>Search Faceit Users </Link></li>
-                        <li><Link to={process.env.PUBLIC_URL + "/service-two"}>Test</Link></li>
-                        <li><Link to={process.env.PUBLIC_URL + "/service-details/design"}>Service Details</Link></li>
+                
                         <li><Link to={process.env.PUBLIC_URL + "/404"}>404 Page</Link></li>
                     </ul>
                 </li>
-                <li className="menu-item-has-children">
+               {/* <li className="menu-item-has-children">
                    	 <Link to="#">Teams <FaAngleDown /></Link>
-                     {/*
+                     
                     <ul className="axil-submenu">
                         <li><Link to={process.env.PUBLIC_URL + "/project-grid-one"}>Two Column</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/project-grid-two"}>Three Column</Link></li>
@@ -37,10 +34,10 @@ const Nav = () => {
                         <li><Link to={process.env.PUBLIC_URL + "/project-width-two"}>Four Column Width</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/project-details/plan-management"}>Portfolio Details</Link></li>
                     </ul>
-                    */}
-                </li>
+                    
+                </li>*/}
                 
-                <li className="menu-item-has-children">
+              {/*  <li className="menu-item-has-children">
                     <Link to="#">Pages <FaAngleDown /></Link>
                     
                     <ul className="axil-submenu">
@@ -58,21 +55,26 @@ const Nav = () => {
                         <li><Link to={process.env.PUBLIC_URL + "/coming-soon"}>Coming Soon</Link></li>
                     </ul>
                     
-                </li>
+                </li>*/}
                 <li className="menu-item-has-children">
                     <Link to="#">Blog <FaAngleDown /></Link>
                     <ul className="axil-submenu">
-                        <li><Link to={process.env.PUBLIC_URL + "/blog-grid"}>Blogs</Link></li>
+                      {/*   <li><Link to={process.env.PUBLIC_URL + "/blog-grid"}>Blogs</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/blog-details/1"}>Standard Post</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/blog-details/2"}>Gallery Post</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/blog-details/3"}>Video Post</Link></li>
                          <li><Link to={process.env.PUBLIC_URL + "#"}>Audio Post</Link></li>
-                        <li><Link to={process.env.PUBLIC_URL + "#"}>Quote Post</Link></li> 
+                        <li><Link to={process.env.PUBLIC_URL + "#"}>Quote Post</Link></li> */}
                     </ul>
                 </li>
                 
                 <li><Link to={process.env.PUBLIC_URL + "/faceitvisuals"}>FACEIT Extension</Link></li>
-                <li><Link to={process.env.PUBLIC_URL + "/contact"}>CS:GO Tutorials</Link></li>
+                <li className="menu-item-has-children">
+                    <Link to="#">CS:GO Tutorials <FaAngleDown /></Link>
+                    <ul className="axil-submenu">
+                <li><Link to={process.env.PUBLIC_URL + "/contact"}>CS:GO Binds</Link></li>
+                </ul>
+                </li>
                 <li><Link to={process.env.PUBLIC_URL + "/contact"}>Contact</Link></li>
             </ul>
         </nav>
